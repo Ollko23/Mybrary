403330df7845bd175338b1c55d0ce2cbcdfd230d
Alex <ollkojp@gmail.com>
2023-02-26 19:59:00 +0100

    index

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
+const bodyParser = require("body-parser")
 
 const indexRouter = require("./routes/index")
+const authorsRouter = require("./routes/authors")
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.set("strictQuery", false);
@@ -26,5 +29,6 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 
 app.use("/", indexRouter)
+app.use("/authors", authorsRouter)
 
 app.listen(process.env.PORT || 3000)
