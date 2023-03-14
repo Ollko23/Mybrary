872fd251111877b29ed817cbc711d054e0808716
Alex <ollkojp@gmail.com>
2023-03-14 09:55:43 +0100

    finished

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,11 +1,24 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-)
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetHight: 150,
-    imageResizeTargetWidth: 100,
-})
-FilePond.parse(document.body);
+const rootSryles = window.getComputedStyle(document.documentElement)
+
+if (rootSryles.getPropertyValue("--book-cover-width-large") != null && rootSryles.getPropertyValue("--book-cover-width-large") != "") {
+    ready()
+} else {
+    document.getElementById("main-css").addEventListener("load", ready)
+}
+
+function ready() {
+    const coverWidth = parseFloat(rootSryles.getPropertyValue("--book-cover-width-large"))
+    const coverAspectRatio = parseFloat(rootSryles.getPropertyValue("--book-cover-aspect-ratio"))
+    const coverHight = coverWidth / coverAspectRatio
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    )
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverAspectRatio,
+        imageResizeTargetHight: coverHight,
+        imageResizeTargetWidth: coverWidth,
+    })
+    FilePond.parse(document.body);
+}
